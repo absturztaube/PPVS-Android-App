59b01ef8917b798fc907167d85898ef1acd96789
absturztaube <absturztaube@gmail.com>
2013-03-16 13:47:07 +0100

    Cleaning up the code... again

--- src/vs/piratenpartei/ch/app/forum/TopicListAdapter.java
+++ src/vs/piratenpartei/ch/app/forum/TopicListAdapter.java
@@ -10,7 +10,6 @@ import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
