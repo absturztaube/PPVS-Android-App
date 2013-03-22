b16f39750eea8fb0126e9f1d155f7af64458b79f
absturztaube <absturztaube@gmail.com>
2013-03-22 17:49:33 +0100

    Fixed displaying Quotes in Topic View, Startet adding Images to Posts

--- src/vs/piratenpartei/ch/app/forum/TopicItem.java
+++ src/vs/piratenpartei/ch/app/forum/TopicItem.java
@@ -7,9 +7,12 @@ import java.util.List;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.xml.sax.XMLReader;
 
 import android.graphics.drawable.Drawable;
+import android.text.Editable;
 import android.text.Html;
+import android.text.Spanned;
 import android.util.Log;
 
 public class TopicItem 
@@ -18,7 +21,7 @@ public class TopicItem
 	
 	private String _author;
 	private Drawable _avatar;
-	private String _content;
+	private Spanned _content;
 	private String _date;
 	
 	public String getAuthor()
@@ -31,7 +34,7 @@ public class TopicItem
 		return this._avatar;
 	}
 	
-	public String getContent()
+	public Spanned getContent()
 	{
 		return this._content;
 	}
@@ -71,7 +74,30 @@ public class TopicItem
 			if(contents.size() > 0)
 			{
 				Element content = contents.get(0);
-				current._content = Html.fromHtml(content.html()).toString();
+				current._content = Html.fromHtml(content.html(), new Html.ImageGetter() {
+					
+					@Override
+					public Drawable getDrawable(String source) {
+						Log.i(TAG, "Getting Image from: " + source);
+						URL src;
+						try {
+							src = new URL(source);
+							Drawable image = Drawable.createFromStream(src.openStream(), source);
+							return image;
+						} catch (IOException e) {
+							Log.w(TAG, e.getMessage());
+							e.printStackTrace();
+						}
+						return null;
+					}
+				}, new Html.TagHandler() {
+					
+					@Override
+					public void handleTag(boolean arg0, String arg1, Editable arg2,
+							XMLReader arg3) {
+						//do nothing
+					}
+				});
 			}
 			Elements dates = post.select(parser.getSelectorPostDate());
 			if(dates.size() > 0)
